28d17966fc8d2a8ba532cb87859f8779cf6f0dba
Markkirakos19 <markkirakos1984@gmail.com>
2025-04-14 19:37:08 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,201 @@
+function ol0() {
+    window.open("https://download2440.mediafire.com/zr2dmkldeb5gj32yQeDDnkUlWPOezh89ehzOpUKS2jFmSkvYZRqvdoyKUp7gZlx9D__qPQNPdocQeOBkp3ehALDFkYTYGReyyQ-aPrQfZeKXuE1fzK5BuIOB1SBGW3KCam9juwTlBud652yWtvzbVXau31vxnk0WOJ0j67_v1gOs/sm6sn3z4kjjqzuu/macOS+Sequoia+ISO+by+techrechard.com.iso")
+}
+function ol1() {
+    window.open("https://archive.org/download/macos-ventura-and-sonoma/MacOS%20Sonoma.iso")
+}
+function ol2() {
+    window.open("https://archive.org/download/macos-ventura-and-sonoma/MacOS%20Ventura.iso")
+}
+function ol3() {
+    window.open("https://archive.org/download/macos_iso/Monterey_12.6.1.iso")
+}
+function ol4() {
+    window.open("https://archive.org/download/macos_iso/BigSur_11.7.1.iso")
+}
+function ol5() {
+    window.open("https://archive.org/download/macos_iso/Catalina_10.15.7.iso")
+}
+function ol6() {
+    window.open("https://archive.org/download/macos_iso/Mojave_10.14.6.iso")
+}
+function ol7() {
+    window.open("https://archive.org/download/macos_iso/HighSierra_10.13.6.iso")
+}
+function ol8() {
+    window.open("https://archive.org/download/macos-sierra-10.12.6-iso/Install-macOS-Sierra-10.12.6_16G29.iso")
+}
+function ol9() {
+    window.open("https://archive.org/download/el-capitan_202405/ElCapitan.iso")
+}
+function ol10() {
+    window.open("https://archive.org/download/osx1010dp1/OS%20X%2010.10%20Yosemite%2014A238.iso")
+}
+function ol11() {
+    window.open("https://download2440.mediafire.com/4mc7f1xf5pogNvqi0qwwCqzFiBaYQ-Efwz2cii63PpE6BSGlAm1yeQjRgzhXZLytJ1zHQVCblk48gZouV4rogrLSm3ZDdZdJYfEKed9jNL92mckmJR3iPJ_2dx7XokdPvSYNCOkVJYIemL17Fm_U5ZQSKzPrNVE4MMhuMJg0fmbM/vhqd5mhg3jfo24k/macOS+X+Mavericks+ISO+by+techrechard.com.iso")
+}
+function ol12() {
+    window.open("https://download2393.mediafire.com/onyhupi3ufqgw5LKn1hP62pMPz0qEnA-ifM_jXQneZupgj-19xZarhSB2CA_5wZHyObRlgacvDEfNuZPCfURiWrLKgci69PnS-iLIxJYjSkyNcj_k4RvFDubrbQF5DQ1DvqzLzTKTXuj-lnZI7L9uWtQyNKkyT23bjccg3xgVprZ/9h82v8h7gphssw1/OS+X+10.8.5+Mountain+Lion+ISO+by+techrechard.com.iso")
+}
+function ol13() {
+    window.open('https://download2393.mediafire.com/onyhupi3ufqglGQVrwnjHAah7B7lbsWyGA9ier8bqVpfoxFRsehD9MDXdbr1DhzR1SfSoWzDiOFJRHQTRIhHnqXq8Q5Zd9Ja1ehIR5Xwv-_we8RYCk31kP3dFuza4t8eAfvZGVjmjabGNoHBoLWTujti_ipnv8xwEyps5MVhyeHJ/9h82v8h7gphssw1/OS+X+10.8.5+Mountain+Lion+ISO+by+techrechard.com.iso')
+}
+function ol14() {
+    window.open('https://dn720507.ca.archive.org/0/items/SnowLeopardInstall/snow%20leopard%20install.iso')
+}
+function ol15() {
+    window.open('https://download2393.mediafire.com/2pbebej1iu4gCXXRSUM9N_7mpOs8Cx_1T07NMJzL-f0qaQGRSUj54XSWveB96HLeyV5WAlYyuBDk0IXbSoV26HKREWiZS8W_RFIWlZB6bbPIp649QxXOwIWhl-igSHLITLkHjoS8GvCwN215XEN9LZNbd6kiJ9QIXEk9MmSETY0RXu4/b7z1ul4wb2yhwc6/Mac_OS_X_Leopard_10.5+ISO+by+techrechard.com.iso')
+}
+function ol16() {
+    window.open('https://archive.org/download/macos-x-10.4-tiger-cd-rom/MacOS%20X%2010.4%20%28Tiger%29%201of4.iso')
+}
+function ol17() {
+    window.open('https://download1532.mediafire.com/07sigr9cn5cgxNrN-Qa0sRSG0zHvPPy4OhFpu4V-qX8G6qcI9kzQVJJWxQBQWlJfB-AtsWYulK1_yr3AElWXUAAzG-LK61gYuXIEXVLeFY9-vHnBZ2CQ4Qt8M-Kfwyn9t-NeJDwRK3GVcMbXt7nlw4cg1Hzbd6H2o5yAYK7UYbshHVM/krucg4l6q6kd9k8/MacOSX10.3-Panther+by+techrechard.com.rar')
+}
+function ol18() {
+    window.open('https://download1326.mediafire.com/o0ird2b4y7pgdp3DD6TB1LQAQGWbrazGZbEJBVCI56Y-uNtI0yKYP2B3ugFjaO0nhB3lZhdREdBo_celjdYh9DDedLxVyxakO_rMfavaa_-VA21UuTS0bIw07omM-qTphDNABXOuU-iWN-8KALNE7K6LEinLcbWxggkv7tYG37S1Fhs/6b67b8vh63cnsjo/macOS+X+Jaguar+10.2+by+techrechard.com.rar')
+}
+function ol19() {
+    window.open('https://download1078.mediafire.com/zoegg0yxkoxgyS5p4dRqkQIBeP6cBhmaxfsUt5IBLxNxyZBshjplFNpmFAW7rtddaIQd21DwIrZC0tiXe8k8k8qQ152fFn0quHMmtuLlffN4Se7ii2pw04RBuC3jD3dSbaJF2CA7B1BUT1T6O3rjBIedmR2vc6onLLUEMu0RkMW9IcU/724w7bofk0jz4gz/Mac+OS+X+10.1+Puma+ISO+by+techrechard.com.iso')}
+function ol20() {
+    window.open('https://download938.mediafire.com/7ywwfvva0trgtk3VQCP79msTBtt7ucE_d3E69uqg2poYm9iBaXTZMGab_qPHOF-S5v2yYrCPAufp_SFwtMyK74_o9ckZ5ivhzwmslDPhw8oeCjt1SXVeA2-678o57OGqrtSQpHmiIS2PVw0BVzlcvAT9gdRLBMvBMQx_Izc1viLUD50/3z5t5e2jjhznb2r/MacosX+Cheetah+10.0+dmg+file+by+techrechard.com.rar')
+}
+
+function ios() {
+    window.location.href = 'ios.html'
+}
+function ipos() {
+    window.location.href = 'ipos.html'
+}
+
+function macos() {
+    window.location.href = 'macos.html'
+}
+
+function oc0() {
+    window.open("https://ipsw.me/install/iPhone12,5/22A3354")
+}
+function oc1() {
+    window.open("https://ipsw.me/install/iPhone12,5/21A329")
+}
+function oc2() {
+    window.open("https://ipsw.me/install/iPhone12,5/20A362")
+}
+function oc3() {
+    window.open("https://ipsw.me/install/iPhone12,5/19A346")
+}
+function oc4() {
+    window.open("https://ipsw.me/install/iPhone12,5/18A373")
+}
+function oc5() {
+    window.open("https://ipsw.me/install/iPhone12,5/17A577")
+}
+function oc6() {
+    window.open("https://updates.cdn-apple.com/2018FallFCS/fullrestores/091-62838/11998604-AC8E-11E8-907D-813265B0C5AC/iPhone_5.5_P3_12.0_16A366_Restore.ipsw")
+}
+function oc7() {
+    window.open("http://appldnld.apple.com/ios11.0/091-31869-201700919-35B22BF6-9027-11E7-AF6F-3535B64D2808/iPhone10,5_11.0_15A372_Restore.ipsw")
+}
+function oc8() {
+    window.open("http://appldnld.apple.com/ios10.0/031-76515-20160907-1706DC08-71FE-11E6-9075-18B934D2D062/iPhone_5.5_10.0.1_14A403_Restore.ipsw")
+}
+function oc9() {
+    window.open("http://appldnld.apple.com/ios9/058-11566-20150916-E5FE2B8A-5678-11E5-ABA0-42F96CA99CB1/iPhone7,1_9.0_13A344_Restore.ipsw")
+}
+function oc10() {
+    window.open("http://appldnld.apple.com/iOS8/031-05555.20140917.YWW0v/iPhone7,1_8.0_12A366_Restore.ipsw")
+}
+function oc11() {
+    window.open("https://ipsw.me/install/iPhone5,2/11A465")
+}
+function oc12() {
+    window.open("https://ipsw.me/install/iPhone5,2/10A405")
+}
+function oc13() {
+    window.open('https://ipsw.me/install/iPhone3,3/9A334')
+}
+function oc14() {
+    window.open('https://ipsw.me/install/iPhone1,2/8A293')
+}
+function oc15() {
+    window.open('https://ipsw.me/install/iPhone1,2/7A341')
+}
+function oc16() {
+    window.open('https://ipsw.me/install/iPhone1,2/5A347')
+}
+function oc17() {
+    window.open('https://ipsw.me/install/iPhone1,1/1A543a')
+}
+
+function re() {
+    window.location.reload()
+}
+
+function ob0() {
+    window.open('https://ipsw.me/install/iPad14,9/22A3354')
+}
+
+function ob1() {
+    window.open('https://ipsw.me/install/iPad13,18/21A329')
+}
+
+function ob2() {
+    window.open('https://ipsw.me/install/iPad13,10/20B82')
+}
+
+function ob3() {
+    window.open('https://ipsw.me/install/iPad13,10/19A346')
+}
+
+function ob4() {
+    window.open('https://ipsw.me/install/iPad8,6/18A373')
+}
+
+function ob5() {
+    window.open('https://ipsw.me/install/iPad8,6/17A844')
+}
+
+function ob6() {
+    window.open('https://ipsw.me/install/iPad8,6/16B92')
+}
+
+function ob7() {
+    window.open('http://appldnld.apple.com/ios11.0/091-31911-201700919-35BB0E7E-9027-11E7-8677-3635B64D2808/iPad_64bit_TouchID_11.0_15A372_Restore.ipsw')
+}
+
+function ob8() {
+    window.open('http://appldnld.apple.com/ios10.0/031-76179-20160907-1701E9AA-71FE-11E6-AC72-0EB934D2D062/iPad_64bit_TouchID_10.0.1_14A403_Restore.ipsw')
+}
+
+function ob9() {
+    window.open('http://appldnld.apple.com/ios9/058-21830-20150916-2CFF2E9A-5678-11E5-8021-E7F76CA99CB1/iPad4,8_9.0_13A344_Restore.ipsw')
+}
+
+function ob10() {
+    window.open('https://ipsw.me/install/iPad2,4/12A365')
+}
+
+function ob11() {
+    window.open('https://ipsw.me/install/iPad2,4/11A465')
+}
+
+function ob12() {
+    window.open('https://ipsw.me/install/iPad2,4/10A403')
+}
+
+function ob13() {
+    window.open('https://ipsw.me/install/iPad2,4/9B176')
+}
+
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    document.body.innerHTML = "<h2>This website is only available on PC.</h2>";
+    function wait(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      
+      wait(3000).then(() => {
+        window.close()
+      });
+  }
